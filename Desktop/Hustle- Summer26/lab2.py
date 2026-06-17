8fc70d48e48885693d1e7bfa11a166ca1d7bd11f
Rebeca Lebron <rebeca@rebecas-mbp.mynetworksettings.com>
2026-06-17 10:57:12 -0700

    week 2 lab 2

---
+++ Desktop/Hustle- Summer26/lab2.py
@@ -0,0 +1,15 @@
+full_name = "Rebeca Lebron"
+
+nickname = "Rebeca"
+
+age = 23
+
+used_python_before = False
+
+hobbies = ["reading" , "photography" , "cooking"]
+
+favorties = { 
+    "food" : "indian food",
+    "hobbie" : "reading"}
+
+print(full_name, age, hobbies)
